c5932da0090d68599184e3e68e6495fc68147578
geovanevirgilio40-gif <geovanevirgilio40@gmail.com>
2026-01-29 22:55:59 +0100

    Delete Projetos/S.A.I/Js/painel.js

--- Projetos/S.A.I/Js/painel.js
+++
@@ -1,9 +0,0 @@
-function pesquisar() {
-  const bi = document.getElementById("bi").value;
-
-  if (bi !== "") {
-    document.getElementById("dados").style.display = "block";
-    document.getElementById("historico").style.display = "block";
-  }
-}
-
